033c012ea91e21d2ffa4d4f25aa21fe670da1d15
AaA <67450869+Awara-Amin@users.noreply.github.com>
2021-05-18 22:42:49 +0300

    Add files via upload

---
+++ src/Review.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import people from './data';
+//  this is was set already but he said is from fontowsome liberary
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+
+console.log (people);
+
+const Review = () => {
+  const [index, setIndex] = useState(2);
+  const {name, job, image, text} = people[index];
+
+  const checkNumber = (number) => {
+    if(number > people.length -1) {
+      return 0
+    }
+    if(number > 0) {
+      return people.length - 1
+  }
+  return number;
+}
+
+const nextPerson = () => {
+  setIndex ((index) => {
+    let newIndex = index + 1;
+    // return newIndex;
+    return checkNumber(newIndex);
+  });
+};
+
+const prevPerson = () => {
+  setIndex ((index) => {
+    let newIndex = index - 1;
+    // return newIndex;
+    return checkNumber(newIndex);
+  });
+}
+
+const randomPerson = () => {
+  // console.log(Math.random())
+  let randomNumber = Math.floor(Math.random() * people.length);
+  console.log(randomNumber);
+
+  if(randomNumber === index){
+    randomNumber = index + 1;
+  }
+  setIndex(checkNumber(randomNumber));
+};
+
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img'></img>
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+
+        <button className='prev-btn'onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+
+        <button className='next-btn' onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+
+      <button className='random-btn' onClick={randomPerson}>
+        <FaChevronLeft />suprise Me</button>
+
+    </article>
+  )
+};
+
+export default Review;
